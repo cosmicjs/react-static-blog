99a2dcc78911cb967b29a4decede8ce8c6738cd7
Sumit Kharche <43902034+sumitkharche@users.noreply.github.com>
2019-06-22 19:01:17 +0530

    added missing config keys

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 export default {
     bucket: {
       slug: process.env.COSMIC_BUCKET || 'coffee-blog',
-      read_key: process.env.COSMIC_READ_KEY || ''
+      read_key: process.env.COSMIC_READ_KEY || '',
+      write_key: process.env.COSMIC_WRITE_KEY || '',
+      port: process.env.PORT || '',  
     }
   }
